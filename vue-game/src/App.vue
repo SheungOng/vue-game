8cb95076599223471cd7d9d2edd7879f34d2469f
Sheung Ong <sheung.ong@osky.com.au>
2023-02-01 20:31:47 +0800

    Commit initial project

---
+++ vue-game/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <GameWorld title="Welcome to Your Game World App"/>
+</template>
+
+<script>
+import GameWorld from './components/GameWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GameWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
